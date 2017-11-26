7495c10b647f6878daeb5209c9d87a419706693a
Jason Reeder <info@jasonreeder.com>
2017-11-26 15:46:45 -0800

    Add buttons to deck

--- components/Deck.js
+++ components/Deck.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alrt, Button, StyleSheet, Text, View } from 'react-native';
 import * as api from '../utils/api';
 
 export default class Deck extends React.Component {
@@ -23,13 +23,22 @@ export default class Deck extends React.Component {
 
   render() {
     const { title } = this.state;
+    const {
+      navigation: { navigate, state: { params: { deckId } } },
+    } = this.props;
     const count = this.calculateCount();
 
     return (
       <View style={styles.container}>
-        <Text>
-          {`${title} - (${count})`}
-        </Text>
+        <Text>{`${title} - (${count})`}</Text>
+        <Button
+          onPress={() => navigate('Quiz', { deckId })}
+          title="start quiz"
+        />
+        <Button
+          onPress={() => navigate('AddCard', { deckId })}
+          title="add card"
+        />
       </View>
     );
   }
