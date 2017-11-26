15a4d19a90f1638dd4a5d3c25c12b42da82a5530
Jason Reeder <info@jasonreeder.com>
2017-11-26 14:21:03 -0800

    Add navigation

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { StackNavigator } from 'react-navigation';
 import { DeckList } from './components';
+
+const Stack = StackNavigator({
+  Home: {
+    screen: DeckList,
+  },
+});
 
 export default class App extends React.Component {
   render() {
-    return <DeckList />;
+    return <Stack />;
   }
 }
