6bd3bc18e01320a80d5228288a71e004ee83f632
Jason Reeder <info@jasonreeder.com>
2017-11-26 13:52:40 -0800

    Start deck view

--- App.js
+++ App.js
@@ -5,9 +5,8 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Animals</Text>
+        <Text>Food</Text>
       </View>
     );
   }
@@ -16,7 +15,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
