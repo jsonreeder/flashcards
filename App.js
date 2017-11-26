e92f60470c01c0a25640f009a292f808dd6f730d
Jason Reeder <info@jasonreeder.com>
2017-11-26 14:05:11 -0800

    Get card counts

--- App.js
+++ App.js
@@ -4,13 +4,13 @@ import * as api from './utils/api';
 
 export default class App extends React.Component {
   render() {
-    const decks = api.getDecks();
+    const decks = api.getDecksInfo();
 
     return (
       <View style={styles.container}>
         {decks.map((d, i) =>
           <Text key={i}>
-            {d.title}
+            {`${d.title} (${d.count})`}
           </Text>,
         )}
       </View>
--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,24 @@
 import seedData from './seedData';
 
-export function getDecks() {
+export function getDecksInfo() {
+  const allDecks = getDecks();
+  return allDecks.map(d => ({
+    title: d.title,
+    count: getCardsForDeck(d.id).length,
+  }));
+}
+
+// Helpers
+
+function getCardsForDeck(deckId) {
+  const allCards = getCards();
+  return allCards.filter(c => c.deck === deckId);
+}
+
+function getDecks() {
   return seedData.decks;
+}
+
+function getCards() {
+  return seedData.cards;
 }
