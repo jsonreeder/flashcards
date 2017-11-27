9c05a517feca74e69291509b1837ab9fec80001f
Jason Reeder <info@jasonreeder.com>
2017-11-26 21:35:45 -0800

    Add input fields

--- components/AddCard.js
+++ components/AddCard.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default class AddCard extends React.Component {
+  state = {
+    front: '',
+    back: '',
+  };
+
   static navigationOptions = () => ({
     title: 'add card',
   });
@@ -9,7 +14,18 @@ export default class AddCard extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Add Card</Text>
+        <Text style={styles.label}>Front</Text>
+        <TextInput
+          onChangeText={front => this.setState({ front })}
+          value={this.state.front}
+          style={styles.input}
+        />
+        <Text style={styles.label}>Back</Text>
+        <TextInput
+          onChangeText={back => this.setState({ back })}
+          value={this.state.back}
+          style={styles.input}
+        />
       </View>
     );
   }
@@ -20,5 +36,15 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 5,
+  },
+  label: {
+    paddingTop: 10,
   },
 });
